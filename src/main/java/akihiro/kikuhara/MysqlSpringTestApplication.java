a69a9ecc8dfcc614fd1d4361fa83c91bf7a0dc2f
菊原 聡宏 <a.kikuhara@alpha.local>
2016-11-07 10:00:50 +0900

    初回コミット

---
+++ src/main/java/akihiro/kikuhara/MysqlSpringTestApplication.java
@@ -0,0 +1,12 @@
+package akihiro.kikuhara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlSpringTestApplication.class, args);
+	}
+}
