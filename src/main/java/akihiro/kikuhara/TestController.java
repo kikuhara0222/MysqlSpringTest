d487c48bfc1f1ae9b63cad56184f901128f9282a
菊原 聡宏 <a.kikuhara@alpha.local>
2016-11-09 12:06:45 +0900

    データの登録

--- src/main/java/akihiro/kikuhara/TestController.java
+++ src/main/java/akihiro/kikuhara/TestController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
@@ -11,16 +13,29 @@ public class TestController {
 	@Autowired
 	UserRepository repository;
 
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Model model) {
-
+		
+		// DBから全件取得
 		Iterable<User> list = repository.findAll();
 		model.addAttribute("results", list);
 
-		for (User user : list) {
-			System.out.println("ID:" + user.getId() + "  NAME:" + user.getName());
-		}
-
 		return "index";
 	}
+	
+	//登録処理
+	@RequestMapping(value="/insert", method = RequestMethod.POST)
+	public String insert(Model model,@RequestParam("insert") String name){
+		System.out.println("1:=============================");
+		
+		User user = new User();
+		user.setName(name);
+		
+		repository.save(user);
+		System.out.println("2:=============================");
+		
+		
+		return "redirect:/";
+	}
+	
 }
